5bcd93a4f2721ffcb157f30160f87858bec7f848
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-12-04 15:50:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-if (initialArray.length > 0) {
-      const transformedData = initialArray.slice(0, initialArray.length - 1).map((value, index) => ({
-        fileId: value,
-        firmId: initialArray[index + 1]
-      }));
-      setRowData(transformedData);
+  const isValidFirmId = (value) => firmIdOptions.some((option) => option.value === value);
+
+  // Transform data to desired format
+  const rowData = initialArray.reduce((acc, item, index) => {
+    // Check if current item and next item are both defined
+    if (item && initialArray[index + 1]) {
+      acc.push({ fileId: item, firmId: isValidFirmId(initialArray[index + 1]) ? initialArray[index + 1] : '' });
+    } else if (item) {
+      // Handle cases with single element or last element
+      acc.push({ fileId: item, firmId: '' });
     }
+    return acc;
+  }
